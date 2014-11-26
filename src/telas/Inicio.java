891d071028d1fd5d8bd2d0a2a5b6ac44c29097fa
leojl <leo.053993@gmail.com>
2014-11-26 15:13:48 -0200

    Revert "Revert "cleiton""
    This reverts commit 73f8a6d77c8350fb17b4fed9d6338bbacfaedb24.

--- src/telas/Inicio.java
+++ src/telas/Inicio.java
@@ -6,10 +6,18 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Label;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Inicio extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtEmail;
 
 	/**
 	 * Launch the application.
@@ -31,12 +39,35 @@ public class Inicio extends JFrame {
 	 * Create the frame.
 	 */
 	public Inicio() {
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 699, 372);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		Label lblEmail = new Label("E-mail");
+		lblEmail.setForeground(Color.BLACK);
+		lblEmail.setFont(new Font("Arial", Font.BOLD, 14));
+		lblEmail.setBounds(199, 95, 62, 22);
+		contentPane.add(lblEmail);
+		
+		txtEmail = new JTextField();
+		txtEmail.setBounds(199, 131, 343, 20);
+		contentPane.add(txtEmail);
+		txtEmail.setColumns(10);
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.setBounds(353, 162, 89, 23);
+		contentPane.add(btnCancelar);
+		
+		JButton btnEntrar = new JButton("Entrar");
+		btnEntrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnEntrar.setBounds(453, 162, 89, 23);
+		contentPane.add(btnEntrar);
 	}
-
 }
