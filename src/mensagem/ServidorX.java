a1c845416090c048e198f5144e6b5240ec52ad2f
leojl <leo.053993@gmail.com>
2014-11-28 01:51:04 -0200

    Alteracao servidorX

--- src/mensagem/ServidorX.java
+++ src/mensagem/ServidorX.java
@@ -10,33 +10,45 @@ import Serializador.Serializador;
 
 public class ServidorX {
 	private ServerSocket server;
-    private int port = 9876;
-    private boolean status = true; 
-     
-    public ServidorX() throws IOException, ClassNotFoundException {
-        server = new ServerSocket(port);
-        while(status){
-            System.out.println("Aguardando cliente");
-            Socket socket = server.accept();
-            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-            Mensagem message = (Mensagem) ois.readObject();
-            Serializador.addEmail(message);
-            
-            System.out.println("Mensagem recebida!\n remetente: "+message.getRemetente());
-            System.out.println("Destinatário: "+message.getDestinatario());
-            System.out.println("Titulo: "+message.getTitulo());
-            System.out.println("Corpo: "+message.getCorpo());
-            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-            oos.writeObject("Ok");
-            ois.close();
-            oos.close();
-            socket.close();
-            //if(message.equalsIgnoreCase("exit")) break;
-        }
-        //System.out.println("Shutting down Socket server!!");
-        //server.close();
-   }
-    public void setStatus(boolean s){
-    	status = s;    	
-    }
+	private int port = 9876;
+	private boolean status = true;
+
+	public ServidorX() throws IOException, ClassNotFoundException {
+		server = new ServerSocket(port);
+		while (status) {
+			System.out.println("Aguardando cliente");
+			Socket socket = server.accept();
+			ObjectInputStream ois = new ObjectInputStream(
+					socket.getInputStream());
+			
+			Object dados = ois.readObject();
+			
+			if(dados instanceof Mensagem){
+				
+				Mensagem message = (Mensagem)dados;
+				Serializador.addEmail(message);
+
+				System.out.println("Mensagem recebida!\n remetente: "
+						+ message.getRemetente());
+				System.out
+						.println("Destinatário: " + message.getDestinatario());
+				System.out.println("Titulo: " + message.getTitulo());
+				System.out.println("Corpo: " + message.getCorpo());
+				ObjectOutputStream oos = new ObjectOutputStream(
+						socket.getOutputStream());
+				oos.writeObject("Ok");
+				ois.close();
+				oos.close();
+				socket.close();
+			}
+			
+			// if(message.equalsIgnoreCase("exit")) break;
+		}
+		// System.out.println("Shutting down Socket server!!");
+		// server.close();
+	}
+
+	public void setStatus(boolean s) {
+		status = s;
+	}
 }
